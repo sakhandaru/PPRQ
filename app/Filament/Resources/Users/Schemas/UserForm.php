7929070ebc3cd8adf8d0df191ca57fd172acdddf
sakhandaru <rifqiagha7@gmail.com>
2025-12-31 14:43:31 +0700

    add: init filament

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->unique(ignoreRecord: true),
+
+            TextInput::make('password')
+                ->password()
+                ->required(fn ($record) => $record === null)
+                ->dehydrateStateUsing(fn ($state) => bcrypt($state))
+                ->hiddenOn('edit'),
+
+            Select::make('role')
+                ->options([
+                    'admin' => 'Admin',
+                    'user'  => 'User',
+                ])
+                ->default('user')
+                ->required(),
+        ]);
+    }
+}
